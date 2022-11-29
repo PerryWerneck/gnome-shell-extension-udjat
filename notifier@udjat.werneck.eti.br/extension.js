6e58abf4b620da8dfc582e466f27829285a5a3bb
Perry Werneck <perry.werneck@gmail.com>
2022-11-29 10:09:45 -0300

    Adding project template.

---
+++ notifier@udjat.werneck.eti.br/extension.js
@@ -0,0 +1,47 @@
+/*
+ * 
+ * This file is part of udjat.
+ * 
+ * dbstatusicon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * dbstatusicon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with dbstatusicon.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ */
+
+/* jshint -W100 */
+/* history */
+/* exported init */
+
+'use strict';
+
+const Main = imports.ui.main;
+
+const UdjatNotifierExtension =
+class UdjatNotifierExtension {
+
+	constructor() {
+	}
+	
+	enable() {
+	}
+	
+	disable() {
+	}
+	
+};
+
+function init() {
+	return new UdjatNotifierExtension();
+}
+
+
+
